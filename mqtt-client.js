91034a42f47a18deed68f7a64b6dac41c4a76950
Hans Tool <hans@defect.org>
2023-06-05 18:25:31 +0200

    Added state return signalling for power switches

--- mqtt-client.js
+++ mqtt-client.js
@@ -1,54 +1,63 @@
-var mqtt = require('mqtt');
-const id = 'signalk-mqtt-client';
-var count = 0;
-var client;
+var mqtt = require('mqtt')
+const id = 'signalk-mqtt-client'
+var count = 0
+var client
 var intervalTime = 10; // seconds between updates
 var TTL = 3600; // default TTL in seconds
 
 module.exports = function (app) {
-  var plugin = {};
-  var paths = {};
-  var updates = [];
-  var updateValues = {};
-  var metas_prev = [];
+  var plugin = {}
+  var paths = {}
+  var updates = []
+  var updateValues = {}
+  var metas_prev = []
+  var unsubscribes = []
+  var subscribePaths = []
+  var states = {}
 
-  plugin.id = 'signalk-mqtt-client';
-  plugin.name = 'Simple MQTT client';
-  plugin.description = 'Simple MQTT client to get updates from MQTT broker';
+  plugin.id = 'signalk-mqtt-client'
+  plugin.name = 'Simple MQTT client'
+  plugin.description = 'Simple MQTT client to get updates from MQTT broker'
 
   plugin.start = function (options, restartPlugin) {
-    app.debug('Options: ' + JSON.stringify(options));
+    app.debug('Options: ' + JSON.stringify(options))
     for (const [key, value] of Object.entries(options.paths)) {
-      paths[value['topic']] = value['path'];
+      paths[value['topic']] = value['path']
     }
-    app.debug('paths: ' + JSON.stringify(paths));
+    app.debug('paths: ' + JSON.stringify(paths))
 
-    const remoteHost = options.remoteHost;
-    client = mqtt.connect(remoteHost,{clientId:"signalk-mqtt-client"});
-    app.debug("Connected flag " + client.connected);
+    // Subscribe to paths
+    let localSubscription = {
+      context: '*',
+      subscribe: []
+    }
+
+    const remoteHost = options.remoteHost
+    client = mqtt.connect(remoteHost,{clientId:"signalk-mqtt-client"})
+    app.debug("Connected flag " + client.connected)
     // Here we put our plugin logic
 
     function toDelta() {
-      context = 'vessels.' + app.selfId;
-      // app.debug('context: ' + context + ' values: ' + JSON.stringify(values));
-      var deltas = [];
-      var metas = [];
-      var epoch = Math.floor(+new Date() / 1000);
+      context = 'vessels.' + app.selfId
+      // app.debug('context: ' + context + ' values: ' + JSON.stringify(values))
+      var deltas = []
+      var metas = []
+      var epoch = Math.floor(+new Date() / 1000)
       for (const [topic, data] of Object.entries(updateValues)) {
-        var path = paths[topic] + '.' + topic.replace(/\//, '.').toLowerCase() + '.';
+        var path = paths[topic] + '.' + topic.replace(/\//, '.').toLowerCase() + '.'
         for (const [key, dataValues] of Object.entries(data)) {
-          var value = dataValues['value'];
-          if (value == 'ON') {
+          var value = dataValues['value']
+          if (value == 'ON' || value == 'Online') {
             value = 1
           }
-          if (value == 'OFF') {
+          if (value == 'OFF' || value == 'Offline') {
             value = 0
           }
-          var valueTTL = dataValues['ttl'];
-          var new_value = value;
-          var units = '';
-          var description = '';
-          app.debug('key: ' + key + ' value: ' + value + ' ttl: ' + valueTTL);
+          var valueTTL = dataValues['ttl']
+          var new_value = value
+          var units = ''
+          var description = ''
+          // app.debug('key: ' + key + ' value: ' + value + ' ttl: ' + valueTTL)
           if (valueTTL > epoch) {
             if (key == 'temperature') {
               new_value = parseFloat((value + 273.15).toFixed(2)); // Celcius to Kelvin
@@ -73,19 +82,22 @@ module.exports = function (app) {
             }
             if (key == 'voltage') {
               if (value > 300) {                                // Fix mV reported
-                new_value = parseFloat((value / 1000).toFixed(3));
+                new_value = parseFloat((value / 1000).toFixed(3))
               }
               units = 'V'
             }
             if (key == 'battery') {
-              new_value = parseFloat((value / 100).toFixed(2));
+              new_value = parseFloat((value / 100).toFixed(2))
               units = 'ratio'
             }
             if (units == "") {
-              deltas.push({path: path + key, value: new_value});
+              deltas.push({path: path + key, value: new_value})
             } else {
-              deltas.push({path: path + key, value: new_value});
-              metas.push({path: path + key, value: {units: units}});
+              deltas.push({path: path + key, value: new_value})
+              metas.push({path: path + key, value: {units: units}})
+            }
+            if (key == 'state' && !path.includes('undefined') && subscribePaths.includes(path+key) == false) {
+              subscribe(path+key, topic)
             }
           }
         }
@@ -102,79 +114,147 @@ module.exports = function (app) {
             values: deltas
           },
         ],
-      };
+      }
       metas_prev = metas.slice()
       return delta
     }
 
+    function subscribe(path, topic) {
+      app.debug('subscribe to path %s', path)
+      subscribePaths.push(path)
+      states[path] = {state: '', stateChange: Date.now(), topic: topic}
+	    localSubscription.subscribe.push({
+	      path: path,
+	      policy: 'instant'
+	    })
+
+	    app.subscriptionmanager.subscribe(
+	      localSubscription,
+	      unsubscribes,
+	      subscriptionError => {
+	        app.error('Error:' + subscriptionError);
+	      },
+	      delta => {
+	        delta.updates.forEach(u => {
+	          handleDelta(u.values)
+	        })
+	      }
+      )
+    }
+
+    // Handle delta
+    function handleDelta (deltas) {
+      deltas.forEach(delta => {
+        // app.debug('delta: %s', delta)
+        // Check for state change of at least a second old
+        if (typeof delta.path != 'undefined' && states[delta.path].state != delta.value) {
+          app.debug('delta changed: %s', delta)
+          if (Date.now() - states[delta.path].stateChange > 1000) {
+            app.debug('delta value changed from %s to %s', states[delta.path], delta.value)
+            if (delta.value == 1) {
+              publish('zigbee2mqtt/' + states[delta.path].topic + '/set', '{"state": "ON"}')
+            } else if (delta.value == 0) {
+              publish('zigbee2mqtt/' + states[delta.path].topic + '/set', '{"state": "OFF"}')
+            }
+            // Store new value
+            states[delta.path].stateChange = Date.now()
+            states[delta.path].state = delta.value
+          } else {
+            app.debug('delta ignored: %s', delta)
+          }
+        }
+      })
+    }
+
+    function publish (topic, message) {
+      let options = {retain:true, qos:1}
+      if (client.connected == true){
+        app.debug('publish: %s %s', topic, message)
+        client.publish(topic, message, options)
+      }
+    }
+
     function sendUpdates () {
-      var updates_copy = JSON.parse(JSON.stringify(updates));
-      app.debug('Sending updates: ' + JSON.stringify(updates_copy));
-      app.handleMessage(id, updates_copy);
+      var updates_copy = JSON.parse(JSON.stringify(updates))
+      app.debug('Sending updates: ' + JSON.stringify(updates_copy))
+      app.handleMessage(id, updates_copy)
     }
 
     function addValues (topic, values) {
       if (!(topic in updateValues)) {
-        updateValues[topic] = {};
+        updateValues[topic] = {}
       }
       for (const [key, value] of Object.entries(values)) {
         if (!(key in updateValues[topic])) {
-          updateValues[topic][key] = {};
+          updateValues[topic][key] = {}
         }
-        updateValues[topic][key]['value'] = value;
-        updateValues[topic][key]['ttl'] = Math.floor(new Date() / 1000) + TTL ;
+        updateValues[topic][key]['value'] = value
+        updateValues[topic][key]['ttl'] = Math.floor(new Date() / 1000) + TTL 
       }
     }
 
-    setInterval(updateAndSend, intervalTime * 1000);
+    setInterval(updateAndSend, intervalTime * 1000)
 
     function updateAndSend () {
-      updates = toDelta();
-      sendUpdates();
+      updates = toDelta()
+      sendUpdates()
     }
 
     //handle incoming messages
     client.on('message',function(topic, message, packet) {
-    	app.debug("Topic is " + topic);
-      app.debug("Message is " + message);
+      message = message.toString()
+    	app.debug("Topic is " + topic)
+      app.debug("Message is %j", message)
       if (topic.match('/bridge/')) {
         app.debug('Skipping topic %s', topic)
       } else {
-        topic = topic.replace(/^[a-zA-Z0-9]+\//,'');
-        var values = JSON.parse(message);
-        addValues(topic, values);
-        updateAndSend();
+        topic = topic.replace(/^[a-zA-Z0-9]+\//,'')
+    	  app.debug("Topic(stripped) is " + topic)
+        var values = {}
+        if (message.startsWith('{')) {
+          // JSONify
+          values = JSON.parse(message)
+        } else {
+          // One value
+          temp = topic.split('/')
+          topic = temp[0]
+          key = temp[1]
+          values[key] = message
+        }
+        app.debug('topic: %s, values: %s', topic, JSON.stringify(values))
+        addValues(topic, values)
+        updateAndSend()
       }
-    });
+    })
 
     client.on("connect",function() {
-      app.debug("Connected " + client.connected);
+      app.debug("Connected " + client.connected)
     })
 
     client.on("end",function() {
-      app.debug("Disconnected at own request.");
+      app.debug("Disconnected at own request.")
     })
 
-    //var topic_list=["zigbee2mqtt/#", "zigbee2mqtt/Douche", "zigbee2mqtt/Woonkamer"];
-    var topic_list=[];
+    //var topic_list=["zigbee2mqtt/#", "zigbee2mqtt/Douche", "zigbee2mqtt/Woonkamer"]
+    var topic_list=[]
     Object.keys(paths).forEach(function(topic) {
-      topic_list.push('+/' + topic);
-    });
-    // topic_list=['#'];
-    // var topic_o={"#":1};
-    app.debug("Subscribing to topics: " + topic_list);
+      topic_list.push('+/' + topic + '/#')
+    })
+    // topic_list=['#']
+    // var topic_o={"#":1}
+    app.debug("Subscribing to topics: " + topic_list)
     // client.subscribe('Buiten',{qos:1}); //single topic
     client.subscribe(topic_list,{qos:1}); //topic list
     // client.subscribe(topic_o); //object
 
-    app.debug('Plugin started');
-  };
+    app.debug('Plugin started')
+  }
 
   plugin.stop = function () {
     // Here we put logic we need when the plugin stops
-    client.end();
-    app.debug('Plugin stopped');
-  };
+    client.end()
+    app.debug('Plugin stopped')
+  }
 
   plugin.schema = {
     // The plugin schema
@@ -216,6 +296,6 @@ module.exports = function (app) {
         },
       },
     }
-  };
-  return plugin;
-};
+  }
+  return plugin
+}
